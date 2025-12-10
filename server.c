40f75c4069cf355e7ee9ddaa9f63562d44ac229e
Swanderson-wiz <102771985+Swanderson-wiz@users.noreply.github.com>
2025-12-10 01:05:41 -0700

    Update server.c

--- server.c
+++ server.c
@@ -32,22 +32,38 @@ int main() {
 		// TODO:
 		// read requests from serverFIFO
 
+		// Read the entire message structure from the server's FIFO.
+		// read() is blocking until a message arrives.
+		ssize_t bytes_read = read(server, &req, sizeof(struct message));
 
-
-
-
+		if (bytes_read == 0) {
+			close(server);
+			server = open("serverFIFO", O_RDONLY);
+			if (server == -1) {
+				break;
+			}//end error if
+			continue;
+		} else if (bytes_read == -1) {
+			break;
+		} else if (bytes_read != sizeof(struct message)) {
+			continue;
+		}//end error ifs
 
 		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
 
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
 		// close target FIFO after writing the message
+		target = open(req.target, O_WRONLY);
 
+		if (target == -1) {
+			continue; 
+		}//end check
 
+		if (write(target, &req, sizeof(struct message)) == -1) {
+		}//end check
 
-
-
-
+		close(target);
 
 	}
 	close(server);
